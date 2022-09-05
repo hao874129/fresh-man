c7b714d9bfa06c97d625f2f028714e393de05f90
hao874129 <80389047+hao874129@users.noreply.github.com>
2022-09-05 14:35:02 +0800

    routerFix

--- src/App.jsx
+++ src/App.jsx
@@ -61,9 +61,10 @@ function App() {
             <Route
               path="*"
               element={
-                <main style={{ padding: "1rem" }}>
-                  <p>{"There's nothing here!"}</p>
-                </main>
+                <Home />
+                // <main style={{ padding: "1rem" }}>
+                //   <p>{"There's nothing here!"}</p>
+                // </main>
               }
             />
 
